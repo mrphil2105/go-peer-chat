1a3b71da3b618375437c45b3ddfc6079799b860c
Philip Mørch <mrphil2105@gmail.com>
2022-12-30 17:04:31 +0100

    Add logging to file

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-var (
-	port = ""
+import (
+	"flag"
+	"fmt"
+	lo "log"
+	"os"
+	"path"
 )
+
+var (
+	name = flag.String("name", "NoName", "Name of the peer")
+	port = ""
+	log  = lo.Default()
+)
+
+func main() {
+	flag.Parse()
+
+	prefix := fmt.Sprintf("%-8s: ", *name)
+	logFileName := path.Join("logs", fmt.Sprintf("%s.log", *name))
+	_ = os.Mkdir("logs", 0664)
+	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+
+	if err != nil {
+		log.Fatalf("Unable to open or create file %s: %v", logFileName, err)
+	}
+
+	log = lo.New(logFile, prefix, lo.Ltime)
+}
--- util.go
+++ util.go
@@ -3,7 +3,6 @@ package main
 import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"log"
 	"net"
 )
 
